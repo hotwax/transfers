472076d00d1cecba4960ce158f41e91adf3c0174
amansinghbais <singh11amanbais@gmail.com>
2025-02-21 16:54:05 +0530

    Improved: cacheing the shipment method types description (#1)

--- src/store/index.ts
+++ src/store/index.ts
@@ -17,7 +17,7 @@ import utilModule from "./modules/util"
 const state: any = {}
 
 const persistState = createPersistedState({
-  paths: ["user", "product.cached", "util.carrierDesc", "util.statusDesc"],
+  paths: ["user", "product.cached", "util.carrierDesc", "util.statusDesc", "util.shipmentMethodTypeDesc"],
   fetchBeforeUse: true
 })
 
