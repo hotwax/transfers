f9b0e14826a14a45b9800619caa9e02d703b6f9b
amansinghbais <singh11amanbais@gmail.com>
2025-02-26 10:56:46 +0530

    Improved: UI of the transfers page to show grouping using accordion (#1)

--- src/views/Transfers.vue
+++ src/views/Transfers.vue
@@ -165,155 +165,155 @@
               <hr />
             </div>
           </template>
-          <template v-else-if="query.groupBy === 'facilityName' || query.groupBy === 'orderFacilityName'">
-            <div v-for="(order, index) in ordersList.orders" :key="index">
-              <section class="list-item">
-                <ion-item lines="none">
-                  <ion-icon slot="start" :icon="query.groupBy === 'facilityName' ? sendOutline : downloadOutline" />
-                  <ion-label>
-                    <strong>{{ order.groupValue }}</strong>
-                    <p>{{ query.groupBy === 'facilityName' ? order.originFacilityId : order.destinationFacilityId }}</p>
-                  </ion-label>
-                </ion-item>
-                <div class="tablet ion-text-center">
-                  <ion-label>
-                    {{ order.totalOrdered || 0 }}
-                    <p>{{ translate("ordered") }}</p>
-                  </ion-label>
-                </div>
-                <div class="tablet ion-text-center">
-                  <ion-label>
-                    {{ order.totalShipped || 0 }}
-                    <p>{{ translate("shipped") }}</p>
-                  </ion-label>
-                </div>
-                <div class="tablet ion-text-center">
-                  <ion-label>
-                    {{ order.totalReceived || 0 }}
-                    <p>{{ translate("received") }}</p>
-                  </ion-label>
-                </div>
-                <div class="ion-padding-end tablet">
-                  <ion-icon :icon="chevronDownOutline" />
-                </div>
-              </section>
-
-              <section>
-                <div class="list-item" v-for="(item, index) in order.doclist.docs" :key="index" @click="router.push(`/order-detail/${item.orderId}`)">
-                  <ion-item lines="none">
-                    <ion-label class="ion-text-wrap">
-                      {{ item.orderName }}
-                      <p>{{ item.orderId }}</p>
-                    </ion-label>
-                  </ion-item>
-                  <ion-chip outline>
-                    <ion-icon :icon="query.groupBy === 'facilityName' ? downloadOutline : sendOutline" />
-                    <ion-label>{{ query.groupBy === "facilityName" ? item.orderFacilityName : item.facilityName }}</ion-label>
-                  </ion-chip>
-                  <div class="tablet ion-text-center">
-                    <ion-label>
-                      {{ item.quantity || 0 }}
-                      <p>{{ translate("ordered") }}</p>
-                    </ion-label>
-                  </div>
-                  <div class="tablet ion-text-center">
-                    <ion-label>
-                      {{ item.shippedQty || 0 }}
-                      <p>{{ translate("shipped") }}</p>
-                    </ion-label>
-                  </div>
-                  <div class="tablet ion-text-center">
-                    <ion-label>
-                      {{ item.receivedQty || 0 }}
-                      <p>{{ translate("received") }}</p>
-                    </ion-label>
-                  </div>
-                  <div class="metadata ion-padding-end">
-                    <ion-note>{{ translate("Created on") }} {{ formatUtcDate(item.orderDate, "dd LLL yyyy") }}</ion-note>
-                    <ion-badge slot="end" :color="getColorByDesc(item.orderItemStatusDesc) || getColorByDesc('default')">{{ item.orderItemStatusDesc }}</ion-badge>
-                  </div>
-                </div>
-              </section>
-              <hr />
-            </div>
-          </template>
           <template v-else>
-            <div v-for="(order, index) in ordersList.orders" :key="index">
-              <section class="list-item">
-                <ion-item lines="none">
-                  <ion-thumbnail slot="start">
-                    <Image :src="getProduct(order.productId)?.mainImageUrl" />
-                  </ion-thumbnail>
-                  <ion-label>
-                    <strong>{{ getProductIdentificationValue(productIdentificationStore.getProductIdentificationPref.primaryId, getProduct(order.productId)) || getProduct(order.productId).productName }}</strong>
-                    <p>{{ getProductIdentificationValue(productIdentificationStore.getProductIdentificationPref.secondaryId, getProduct(order.productId)) }}</p>
-                  </ion-label>
-                </ion-item>
-                <ion-chip outline>
-                  <ion-icon :icon="query.groupBy === 'originFacilityProductId' ? sendOutline : downloadOutline" />
-                  <ion-label>{{ query.groupBy === "originFacilityProductId" ? order.originFacilityName : order.destinationFacilityName }}</ion-label>
-                </ion-chip>
-                <div class="tablet ion-text-center">
-                  <ion-label>
-                    {{ order.totalOrdered || 0 }}
-                    <p>{{ translate("ordered") }}</p>
-                  </ion-label>
-                </div>
-                <div class="tablet ion-text-center">
-                  <ion-label>
-                    {{ order.totalShipped || 0 }}
-                    <p>{{ translate("shipped") }}</p>
-                  </ion-label>
-                </div>
-                <div class="tablet ion-text-center">
-                  <ion-label>
-                    {{ order.totalReceived || 0 }}
-                    <p>{{ translate("received") }}</p>
-                  </ion-label>
-                </div>
-                <div class="ion-padding-end tablet">
-                  <ion-icon :icon="chevronDownOutline" />
-                </div>
-              </section>
-
-              <section>
-                <div class="list-item" v-for="(item, index) in order.doclist.docs" :key="index" @click="router.push(`/order-detail/${item.orderId}`)">
-                  <ion-item lines="none">
-                    <ion-label class="ion-text-wrap">
-                      {{ item.orderName }}
-                      <p>{{ item.orderId }}</p>
-                    </ion-label>
-                  </ion-item>
-                  <ion-chip outline>
-                    <ion-icon :icon="query.groupBy === 'originFacilityProductId' ? downloadOutline : sendOutline" />
-                    <ion-label>{{ query.groupBy === "originFacilityProductId" ? item.orderFacilityName : item.facilityName }}</ion-label>
-                  </ion-chip>
-                  <div class="tablet ion-text-center">
-                    <ion-label>
-                      {{ item.quantity || 0 }}
-                      <p>{{ translate("ordered") }}</p>
-                    </ion-label>
-                  </div>
-                  <div class="tablet ion-text-center">
-                    <ion-label>
-                      {{ item.shippedQty || 0 }}
-                      <p>{{ translate("shipped") }}</p>
-                    </ion-label>
-                  </div>
-                  <div class="tablet ion-text-center">
-                    <ion-label>
-                      {{ item.receivedQty || 0 }}
-                      <p>{{ translate("received") }}</p>
-                    </ion-label>
-                  </div>
-                  <div class="metadata ion-padding-end">
-                    <ion-note>{{ translate("Created on") }} {{ formatUtcDate(item.orderDate, "dd LLL yyyy") }}</ion-note>
-                    <ion-badge slot="end" :color="getColorByDesc(item.orderItemStatusDesc) || getColorByDesc('default')">{{ item.orderItemStatusDesc }}</ion-badge>
-                  </div>
-                </div>
-              </section>
-              <hr />
-            </div>
+            <ion-accordion-group :multiple="true">
+              <template v-for="(order, index) in ordersList.orders" :key="index">
+                <ion-accordion :value="index">
+                  <template v-if="query.groupBy === 'facilityName' || query.groupBy === 'orderFacilityName'">
+                    <div class="list-item" slot="header" color="light">
+                      <ion-item lines="none">
+                        <ion-icon slot="start" :icon="query.groupBy === 'facilityName' ? sendOutline : downloadOutline" />
+                        <ion-label>
+                          <strong>{{ order.groupValue }}</strong>
+                          <p>{{ query.groupBy === 'facilityName' ? order.originFacilityId : order.destinationFacilityId }}</p>
+                        </ion-label>
+                      </ion-item>
+                      <div class="tablet ion-text-center">
+                        <ion-label>
+                          {{ order.totalOrdered || 0 }}
+                          <p>{{ translate("ordered") }}</p>
+                        </ion-label>
+                      </div>
+                      <div class="tablet ion-text-center">
+                        <ion-label>
+                          {{ order.totalShipped || 0 }}
+                          <p>{{ translate("shipped") }}</p>
+                        </ion-label>
+                      </div>
+                      <div class="tablet ion-text-center">
+                        <ion-label>
+                          {{ order.totalReceived || 0 }}
+                          <p>{{ translate("received") }}</p>
+                        </ion-label>
+                      </div>
+                      <div class="ion-padding-end">
+                        <ion-icon :icon="chevronDownOutline" class="ion-accordion-toggle-icon" />
+                      </div>
+                    </div>
+                    <div class="ion-padding" slot="content">
+                      <div class="list-item" v-for="(item, index) in order.doclist.docs" :key="index" @click="router.push(`/order-detail/${item.orderId}`)">
+                        <ion-item lines="none">
+                          <ion-label class="ion-text-wrap">
+                            {{ item.orderName }}
+                            <p>{{ item.orderId }}</p>
+                          </ion-label>
+                        </ion-item>
+                        <ion-chip outline>
+                          <ion-icon :icon="query.groupBy === 'facilityName' ? downloadOutline : sendOutline" />
+                          <ion-label>{{ query.groupBy === "facilityName" ? item.orderFacilityName : item.facilityName }}</ion-label>
+                        </ion-chip>
+                        <div class="tablet ion-text-center">
+                          <ion-label>
+                            {{ item.quantity || 0 }}
+                            <p>{{ translate("ordered") }}</p>
+                          </ion-label>
+                        </div>
+                        <div class="tablet ion-text-center">
+                          <ion-label>
+                            {{ item.shippedQty || 0 }}
+                            <p>{{ translate("shipped") }}</p>
+                          </ion-label>
+                        </div>
+                        <div class="tablet ion-text-center">
+                          <ion-label>
+                            {{ item.receivedQty || 0 }}
+                            <p>{{ translate("received") }}</p>
+                          </ion-label>
+                        </div>
+                        <div class="metadata ion-padding-end">
+                          <ion-note>{{ translate("Created on") }} {{ formatUtcDate(item.orderDate, "dd LLL yyyy") }}</ion-note>
+                          <ion-badge slot="end" :color="getColorByDesc(item.orderItemStatusDesc) || getColorByDesc('default')">{{ item.orderItemStatusDesc }}</ion-badge>
+                        </div>
+                      </div>
+                    </div>
+                  </template>
+                  <template v-else>
+                    <div class="list-item" slot="header">
+                      <ion-item lines="none">
+                        <ion-thumbnail slot="start">
+                          <Image :src="getProduct(order.productId)?.mainImageUrl" />
+                        </ion-thumbnail>
+                        <ion-label>
+                          <strong>{{ getProductIdentificationValue(productIdentificationStore.getProductIdentificationPref.primaryId, getProduct(order.productId)) || getProduct(order.productId).productName }}</strong>
+                          <p>{{ getProductIdentificationValue(productIdentificationStore.getProductIdentificationPref.secondaryId, getProduct(order.productId)) }}</p>
+                        </ion-label>
+                      </ion-item>
+                      <ion-chip outline>
+                        <ion-icon :icon="query.groupBy === 'originFacilityProductId' ? sendOutline : downloadOutline" />
+                        <ion-label>{{ query.groupBy === "originFacilityProductId" ? order.originFacilityName : order.destinationFacilityName }}</ion-label>
+                      </ion-chip>
+                      <div class="tablet ion-text-center">
+                        <ion-label>
+                          {{ order.totalOrdered || 0 }}
+                          <p>{{ translate("ordered") }}</p>
+                        </ion-label>
+                      </div>
+                      <div class="tablet ion-text-center">
+                        <ion-label>
+                          {{ order.totalShipped || 0 }}
+                          <p>{{ translate("shipped") }}</p>
+                        </ion-label>
+                      </div>
+                      <div class="tablet ion-text-center">
+                        <ion-label>
+                          {{ order.totalReceived || 0 }}
+                          <p>{{ translate("received") }}</p>
+                        </ion-label>
+                      </div>
+                      <div class="ion-padding-end tablet">
+                        <ion-icon :icon="chevronDownOutline" class="ion-accordion-toggle-icon" />
+                      </div>
+                    </div>
+                    <div class="ion-padding" slot="content">
+                      <div class="list-item" v-for="(item, index) in order.doclist.docs" :key="index" @click="router.push(`/order-detail/${item.orderId}`)">
+                        <ion-item lines="none">
+                          <ion-label class="ion-text-wrap">
+                            {{ item.orderName }}
+                            <p>{{ item.orderId }}</p>
+                          </ion-label>
+                        </ion-item>
+                        <ion-chip outline>
+                          <ion-icon :icon="query.groupBy === 'originFacilityProductId' ? downloadOutline : sendOutline" />
+                          <ion-label>{{ query.groupBy === "originFacilityProductId" ? item.orderFacilityName : item.facilityName }}</ion-label>
+                        </ion-chip>
+                        <div class="tablet ion-text-center">
+                          <ion-label>
+                            {{ item.quantity || 0 }}
+                            <p>{{ translate("ordered") }}</p>
+                          </ion-label>
+                        </div>
+                        <div class="tablet ion-text-center">
+                          <ion-label>
+                            {{ item.shippedQty || 0 }}
+                            <p>{{ translate("shipped") }}</p>
+                          </ion-label>
+                        </div>
+                        <div class="tablet ion-text-center">
+                          <ion-label>
+                            {{ item.receivedQty || 0 }}
+                            <p>{{ translate("received") }}</p>
+                          </ion-label>
+                        </div>
+                        <div class="metadata ion-padding-end">
+                          <ion-note>{{ translate("Created on") }} {{ formatUtcDate(item.orderDate, "dd LLL yyyy") }}</ion-note>
+                          <ion-badge slot="end" :color="getColorByDesc(item.orderItemStatusDesc) || getColorByDesc('default')">{{ item.orderItemStatusDesc }}</ion-badge>
+                        </div>
+                      </div>
+                    </div>
+                  </template>
+                </ion-accordion>
+              </template>
+            </ion-accordion-group>
           </template>
 
           <ion-infinite-scroll @ionInfinite="loadMoreOrders($event)" threshold="100px" v-show="isScrollable" ref="infiniteScrollRef">
@@ -332,7 +332,7 @@
 </template>
 
 <script setup lang="ts">
-import { IonBadge, IonButtons, IonChip, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonInfiniteScroll, IonInfiniteScrollContent, IonItem, IonLabel, IonList, IonMenuButton, IonNote, IonPage, IonSearchbar, IonSelect, IonSelectOption, IonSpinner, IonThumbnail, IonTitle, IonToolbar, onIonViewWillEnter } from '@ionic/vue';
+import { IonAccordion, IonAccordionGroup, IonBadge, IonButtons, IonChip, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonInfiniteScroll, IonInfiniteScrollContent, IonItem, IonLabel, IonList, IonMenuButton, IonNote, IonPage, IonSearchbar, IonSelect, IonSelectOption, IonSpinner, IonThumbnail, IonTitle, IonToolbar, onIonViewWillEnter } from '@ionic/vue';
 import { addOutline, arrowUpOutline, chevronDownOutline, documentTextOutline, downloadOutline, filterOutline, sendOutline, swapVerticalOutline } from 'ionicons/icons';
 import { getProductIdentificationValue, translate, useProductIdentificationStore } from '@hotwax/dxp-components'
 import router from '@/router';
@@ -453,6 +453,10 @@ main > div{
 which results in distorted label text and thus reduced ion-item width */
 .list-item > ion-item {
   width: 100%;
+}
+
+ion-accordion {
+  border-bottom: var(--border-medium);
 }
 
 @media (min-width: 991px) {
