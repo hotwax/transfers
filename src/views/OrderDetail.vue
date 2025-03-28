2a280fc5f0b9de90f4d454efaef6d5f93da14e72
amansinghbais <singh11amanbais@gmail.com>
2025-03-28 12:51:20 +0530

    Improved: optimizrd logic for displaying facility address on UI (#31)

--- src/views/OrderDetail.vue
+++ src/views/OrderDetail.vue
@@ -36,13 +36,12 @@
               </ion-card-header>
               <ion-item lines="none">
                 <ion-icon :icon="sendOutline" slot="start" />
-                <ion-label v-if="currentOrder.originFacility?.address1">
-                  <h3>{{ currentOrder.originFacility.address1 }}</h3>
+                <ion-label>
+                  <h3 v-if="currentOrder.originFacility?.address1">{{ currentOrder.originFacility.address1 }}</h3>
                   <h3 v-if="currentOrder.originFacility?.address2">{{ currentOrder.originFacility.address2 }}</h3>
-                  <p>{{ (currentOrder.originFacility?.city && currentOrder.originFacility?.postalCode) ? `${currentOrder.originFacility.city}, ${currentOrder.originFacility.postalCode}` : (currentOrder.originFacility?.city) ? currentOrder.originFacility.city : (currentOrder.originFacility?.postalCode || "") }}</p>
-                  <p>{{ (currentOrder.originFacility?.stateGeoName && currentOrder.originFacility?.countryGeoName) ? `${currentOrder.originFacility.stateGeoName}, ${currentOrder.originFacility.countryGeoName}` : (currentOrder.originFacility?.stateGeoName) ? currentOrder.originFacility?.stateGeoName : (currentOrder.originFacility?.countryGeoName || "") }}</p>
-                </ion-label>
-                <ion-label v-else>-</ion-label>
+                  <p>{{ currentOrder.originFacility?.city ?? "" }}{{ currentOrder.originFacility?.postalCode && ", " }}{{ currentOrder.originFacility?.postalCode ?? "" }}</p>
+                  <p>{{ currentOrder.originFacility?.stateGeoName ?? "" }}{{ currentOrder.originFacility?.countryGeoName && ", " }}{{ currentOrder.originFacility?.countryGeoName ?? "" }}</p>
+                  </ion-label>
               </ion-item>
               <ion-item>
                 <ion-select :label="translate('Carrier')" :value="currentOrder.carrierPartyId" interface="popover" :placeholder="translate('Select')" :disabled="isOrderFinished()" @ionChange="updateCarrierAndShipmentMethod($event, $event.detail.value, '')">
@@ -66,13 +65,12 @@
               </ion-card-header>
               <ion-item lines="none">
                 <ion-icon :icon="downloadOutline" slot="start" />
-                <ion-label v-if="currentOrder.destinationFacility?.address1">
-                  <h3>{{ currentOrder.destinationFacility.address1 }}</h3>
+                <ion-label>
+                  <h3 v-if="currentOrder.destinationFacility?.address1">{{ currentOrder.destinationFacility.address1 }}</h3>
                   <h3 v-if="currentOrder.destinationFacility?.address2">{{ currentOrder.destinationFacility.address2 }}</h3>
-                  <p>{{ (currentOrder.destinationFacility?.city && currentOrder.destinationFacility?.postalCode) ? `${currentOrder.destinationFacility.city}, ${currentOrder.destinationFacility.postalCode}` : (currentOrder.destinationFacility?.city) ? currentOrder.destinationFacility.city : (currentOrder.destinationFacility?.postalCode || "") }}</p>
-                  <p>{{ (currentOrder.destinationFacility?.stateGeoName && currentOrder.destinationFacility?.countryGeoName) ? `${currentOrder.destinationFacility.stateGeoName}, ${currentOrder.destinationFacility.countryGeoName}` : (currentOrder.destinationFacility?.stateGeoName) ? currentOrder.destinationFacility?.stateGeoName : (currentOrder.destinationFacility?.countryGeoName || "") }}</p>
+                  <p>{{ currentOrder.destinationFacility?.city ?? "" }}{{ currentOrder.destinationFacility?.postalCode && ", " }}{{ currentOrder.destinationFacility?.postalCode ?? "" }}</p>
+                  <p>{{ currentOrder.destinationFacility?.stateGeoName ?? "" }}{{ currentOrder.destinationFacility?.countryGeoName && ", " }}{{ currentOrder.destinationFacility?.countryGeoName ?? "" }}</p>
                 </ion-label>
-                <ion-label v-else>-</ion-label>
               </ion-item>
             </ion-card>
           </div>
